b04c6244ba26ca37cb5f2d4bf4b8e50eeccc0c22
cw <fromnowwon@gmail.com>
2022-03-25 17:42:37 +0900

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import { useLocation } from 'react-router';
 import { modeHandler } from './_actions/mode_actions';
 import { useDispatch, useSelector } from "react-redux";
 import RouteChangeTracker from './components/RouteChangeTracker';
+import Loader from '../src/components/views/commons/Loader';
 
 let LandingPage = React.lazy(() => { return import('./components/views/pages/LandingPage/LandingPage') })
 let AboutPage = React.lazy(() => { return import('./components/views/pages/AboutPage/AboutPage') })
@@ -49,32 +50,32 @@ const App = ():JSX.Element => {
 			<NavBar />
 			<Routes>
 				<Route path="/" element={ 
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<LandingPage /> 
 					</React.Suspense>
 				}/>
 				<Route path="/about" element={ 
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<AboutPage /> 
 					</React.Suspense>
 				}/>
 				<Route path="/lab" element={
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<LabPage /> 
 					</React.Suspense>
 				}/>
 				<Route path="/contact" element={ 
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<ContactPage />
 					</React.Suspense>
 				}/>
 				<Route path="/login" element={ 
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<LoginPage />
 					</React.Suspense>
 				}/>
 				<Route path="/register" element={ 
-					<React.Suspense fallback={ <div className="fallback-message">Loading...</div> }>
+					<React.Suspense fallback={ <Loader /> }>
 						<RegisterPage />
 					</React.Suspense>
 				}/>
